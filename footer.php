ce80e6092ee976f2190e6194f90235f854a6b95a
arthurbergere <arbergere@stpbb.org>
2022-02-04 09:55:57 +0100

    base

---
+++ footer.php
@@ -0,0 +1,6 @@
+<footer>
+        <p>© 2022 La Mouillère - Tous droits réservés</p>
+    </footer>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -1,14 +1,19 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
     <meta charset="UTF-8">
-    <title><?php bloginfo('name')?> - <?php bloginfo('description')?>  </title>
+    <title><?php bloginfo('name') ?> - <?php bloginfo('description') ?> </title>
 </head>
+
 <body>
-    <?php while (have_posts(  )) : the_post(  ); ?>
-    <h1><?php the_title(); ?></h1>
-    <?php the_content(); ?>
-    <?php endwhile; ?>
-</body>
-</html>
+    <?php get_header(); ?>
+    <main>
+        <?php while (have_posts()) : the_post(); ?>
+            <h1><?php the_title(); ?></h1>
+            <?php the_content(); ?>
+        <?php endwhile; ?>
+    </main>
+    <?php get_footer(); ?>
+
